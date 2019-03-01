0dc77bfc6444cfdb2b605b2bc66238194c69031c
Ryosuke IWANAGA <me@riywo.com>
2019-02-28 23:37:17 -0800

    Add JUnit and AssertJ

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,18 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.4.0")
+    testCompile("org.assertj:assertj-core:3.11.1")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.4.0")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    systemProperties = mapOf(
+        "junit.jupiter.testinstance.lifecycle.default" to "per_class"
+    )
 }

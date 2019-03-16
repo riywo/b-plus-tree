e5ab6b575dfce9928752f1374ff411b7fc7258f6
Ryosuke IWANAGA <me@riywo.com>
2019-03-16 01:45:19 -0700

    wi

--- src/main/kotlin/InternalNode.kt
+++ src/main/kotlin/InternalNode.kt
@@ -1,5 +1,7 @@
 package com.riywo.ninja.bptree
 
+import java.lang.Exception
+
 class InternalNode(table: Table, page: AvroPage) : Node(table.key, table.internal, page) {
     fun findChildPageId(key: AvroGenericRecord): Int {
         val result = find(key)
@@ -21,5 +23,23 @@ class InternalNode(table: Table, page: AvroPage) : Node(table.key, table.interna
         val internal = createRecord(leftNode.records.last())
         internal.put(INTERNAL_ID_FIELD_NAME, leftNode.id)
         page.insert(0, internal.toByteBuffer())
+    }
+
+    fun addChildNode(node: Node) {
+        val internal = createRecord(node.records.last())
+        val result = find(internal)
+        when (result) {
+            is FindResult.LeftMatch -> {
+                // TODO
+            }
+            is FindResult.RightMatch -> {
+                if (page.sentinelId == null) { // Initialization
+                    page.sentinelId = node.id
+                } else {
+                    // TODO
+                }
+            }
+            is FindResult.ExactMatch -> throw Exception()
+        }
     }
 }
